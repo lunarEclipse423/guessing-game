35fc2a5ef305477913983f3819cebae099bfb485
Maria White <redstone.mari@gmail.com>
2022-08-14 16:38:09 +0300

    feat: solve guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.candidate = this.min + Math.ceil((this.max - this.min) / 2);
+        console.log(this.candidate);
     }
 
     guess() {
-
+        return this.candidate;
     }
 
     lower() {
-
+        this.setRange(this.min, this.candidate);
     }
 
     greater() {
-
+        this.setRange(this.candidate, this.max);
     }
 }
 
